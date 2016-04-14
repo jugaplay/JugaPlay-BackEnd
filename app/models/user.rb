d1c77ee59cbef368521edc2560f7aea22bf55ef1
florencia <torielli.florencia@gmail.com>
2016-04-13 23:39:15 -0300

    Se agrega condicion en caso de no tener un anfitrion para login facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,8 +58,10 @@ class User < ActiveRecord::Base
     user_by_email = find_by(email: auth.info.email)
     
     if not user_by_email.present?
-      @host_user = User.find( params["invited_by"])
+    if params.has_key?(:invited_by)
+      @host_user = User.find(params["invited_by"])
  	  @host_user.win_coins!(10)
+ 	end
  	end
  	
     return user_by_email if user_by_email.present?
@@ -73,26 +75,8 @@ class User < ActiveRecord::Base
       user.wallet = Wallet.new
     end
     
- 
-    
   end
 
-
-  def self.from_omniauth(auth)
-    # TODO: Move all this shit outta here
-
-    user_by_email = find_by(email: auth.info.email)
-    return user_by_email if user_by_email.present?
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.email = auth.info.email
-      user.nickname = auth.uid
-      user.password = Devise.friendly_token[0,20]
-      user.image = auth.info.image
-      user.wallet = Wallet.new
-    end
-  end
 
   private
 
