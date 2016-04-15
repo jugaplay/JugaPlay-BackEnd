7af7018b6b44b29001a8201970970b80bf63973e
florencia <torielli.florencia@gmail.com>
2016-04-15 18:31:37 -0300

    Nickname amigable

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,7 +69,7 @@ class User < ActiveRecord::Base
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.email = auth.info.email
-      user.nickname = auth.uid
+      user.nickname = auth.info.email.split("@").first + Random.rand(999).to_s
       user.password = Devise.friendly_token[0,20]
       user.image = auth.info.image
       user.wallet = Wallet.new
