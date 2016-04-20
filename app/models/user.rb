ba7b638eb7eb730c127355417c7b744910fee2ee
florencia <torielli.florencia@gmail.com>
2016-04-20 20:43:44 -0300

    Fix facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,7 +72,7 @@ class User < ActiveRecord::Base
       if(auth.info.email.present?)
       	user.nickname = auth.info.email.split("@").first + Random.rand(999).to_s
       else
-     	 user.nickname = @auth.uid
+     	 user.nickname = auth.uid
       end
       user.password = Devise.friendly_token[0,20]
       user.image = auth.info.image
