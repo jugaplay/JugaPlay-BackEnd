5281e3b5bceda05695aa8cf8ec00535ce4b92ed6
florencia <torielli.florencia@gmail.com>
2016-04-20 20:12:46 -0300

    Facebook

--- app/controllers/api/v1/users/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/users/omniauth_callbacks_controller.rb
@@ -5,18 +5,19 @@ class Api::V1::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksCon
 	 @user = User.from_omniauth(request.env['omniauth.auth'],request.env['omniauth.params'])
     	
 	if @user.persisted?
-      sign_in @user     
+      sign_in @user
+      redirect_to "http://#{ENV['DOMAIN_NAME']}"
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
+      render json: { errors: @user.errors }
     end
     
-     redirect_to "http://#{ENV['DOMAIN_NAME']}"
-     
   end
   
   
  def failure
-    render json: { errors: 'There was an error trying to login with facebook' }
+     redirect_to "http://#{ENV['DOMAIN_NAME']}" 
+     #render json: { errors: 'There was an error trying to login with facebook' }
   end
   
   
