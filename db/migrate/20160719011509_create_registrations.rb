89aac46aedd31d9c85fa3bae56f2d5b7ecf47c51
florencia <torielli.florencia@gmail.com>
2016-07-22 19:53:03 -0300

    Agregado de detail

--- db/migrate/20160719011509_create_registrations.rb
+++ db/migrate/20160719011509_create_registrations.rb
@@ -3,10 +3,11 @@ class CreateRegistrations < ActiveRecord::Migration
     create_table :registrations do |t|
       t.integer :won_coins
       t.inet :guest_ip
+      t.string: detail
       t.references :registration_status, null: false
       t.references :request, null: false
       t.references :guest_user, null: true
-
+	
       t.timestamps null: false
     end
               
