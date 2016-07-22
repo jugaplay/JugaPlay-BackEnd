9c070c4574e322b6d3c1f92d41bd62b4e19735a2
florencia <torielli.florencia@gmail.com>
2016-07-22 01:04:24 -0300

    Validacion para que cada nuevo registro solo asigne monedas una vez.

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -9,14 +9,19 @@ class Api::V1::RegistrationsController < Api::BaseController
   end
   
   def create
-  	
+
 	@request = Request.find(params[:request_id])
 	@registration_status = RegistrationStatus.find(params[:registration_status_id])
+	
 	if params[:guest_user_id].present?
 	 @guest_user = User.find(params[:guest_user_id])
+	 @request.host_user.win_coins!(params[:won_coins])
 	end
+    
     @registration = Registration.create(registration_status:@registration_status , request: @request, won_coins: params[:won_coins], guest_user: @guest_user)
-    render :show
+	
+    	return render :show if(@registration.valid?)
+        render_json_errors @registration.errors   
   
   end
 
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,4 +2,6 @@ class Registration < ActiveRecord::Base
   belongs_to :registration_status
   belongs_to :request
   belongs_to :guest_user , :class_name => 'User',  required: false
+  
+  validates :guest_user, uniqueness: true
 end
