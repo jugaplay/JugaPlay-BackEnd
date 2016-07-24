df262665bac757f9165b9fde79fd7fc5215669c5
florencia <torielli.florencia@gmail.com>
2016-07-24 11:18:06 -0300

    Contenido del mail

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -2,13 +2,12 @@ require 'digest/sha2'
   
 class InvitationsMailer < ActionMailer::Base
 		
-   default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@jugaplay.com"
-	
 			
   def send_mail(to, from, content, from_user_id, sender_link)
  
-   @user = User.find(from_user_id)
+    @user = User.find(from_user_id)
 	@sender_link = sender_link
+	@params = content
 	
 	mail to: to, from: from,
       subject: "#{@user.nickname} te ha invitado a jugar a JugaPlay" do |format|
