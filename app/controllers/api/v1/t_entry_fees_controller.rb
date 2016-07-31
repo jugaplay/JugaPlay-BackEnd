996ac8344342ab644b591bd4786f041cb2657b57
florencia <torielli.florencia@gmail.com>
2016-07-31 20:09:25 -0300

    POST api/v1/t_entry_fee

---
+++ app/controllers/api/v1/t_entry_fees_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::TEntryFeesController < Api::BaseController
+ 
+
+	def create
+		@table = Table.where(id: params[:table_id]).first()
+		@tournament = Tournament.where(id: params[:tournament_id]).first()
+	    @t_entry_fee = TEntryFee.new(coins: params[:coins],detail: params[:detail], 
+	    user: current_user, table: @table, tournament: @tournament)
+	    
+		  return render :show if  @t_entry_fee.save
+  		  render_json_errors @t_entry_fee.errors
+    
+	end
+	  		
+end
