5428ff50c0236ac34e62d6b4dca2202b58d57c3a
florencia <torielli.florencia@gmail.com>
2016-07-31 19:04:32 -0300

    scripts migracion y creacion de tablas en produccion

--- Documentacion/creacion_tablas_jul-16.sql
+++ Documentacion/creacion_tablas_jul-16.sql
@@ -8,6 +8,7 @@ CREATE TABLE explanations
   id serial NOT NULL,
   name character varying,
   detail text,
+   active boolean,
   created_at timestamp without time zone NOT NULL,
   updated_at timestamp without time zone NOT NULL,
   CONSTRAINT explanations_pkey PRIMARY KEY (id)
@@ -37,6 +38,7 @@ CREATE TABLE explanations_users
   id serial NOT NULL,
   user_id integer NOT NULL,
   explanation_id integer NOT NULL,
+  active boolean NOT NULL DEFAULT true,
   created_at timestamp without time zone NOT NULL,
   updated_at timestamp without time zone NOT NULL,
   CONSTRAINT explanations_users_pkey PRIMARY KEY (id)
@@ -312,73 +314,247 @@ CREATE INDEX index_notifications_on_user_id
 
 
 
+-- Table: languages
+
+-- DROP TABLE languages;
+
+CREATE TABLE languages
+(
+  id serial NOT NULL,
+  name character varying,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  CONSTRAINT languages_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE languages
+  OWNER TO ucfdkl13ogoaal;
+
+-- Index: index_languages_on_name
+
+-- DROP INDEX index_languages_on_name;
+
+CREATE UNIQUE INDEX index_languages_on_name
+  ON languages
+  USING btree
+  (name COLLATE pg_catalog."default");
 
 
-INSERT INTO 
-channels(
-user_id, mail, sms, 
-whatsapp, push, created_at, updated_at)
 
-SELECT 
+-- Table: countries
 
- id,
+-- DROP TABLE countries;
 
- CASE WHEN (users.email is not null)
- THEN true
- ELSE false
- END AS mail,
+CREATE TABLE countries
+(
+  id serial NOT NULL,
+  name character varying,
+  language_id integer,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  CONSTRAINT countries_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_rails_6f479b409c FOREIGN KEY (language_id)
+      REFERENCES languages (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE countries
+  OWNER TO ucfdkl13ogoaal;
 
- CASE WHEN (users.telephone is not null)
- THEN true
- ELSE false
- END AS sms,
+-- Index: index_countries_on_language_id
 
- CASE WHEN (users.telephone is not null)
- THEN true
- ELSE false
- END AS whatsapp,
+-- DROP INDEX index_countries_on_language_id;
 
- CASE WHEN (users.push_token is not null)
- THEN true
- ELSE false
- END AS push ,
+CREATE INDEX index_countries_on_language_id
+  ON countries
+  USING btree
+  (language_id);
 
- Now(),
- Now()
+-- Index: index_countries_on_name
 
- FROM users 
-;
+-- DROP INDEX index_countries_on_name;
 
-
-INSERT INTO notification_types (
-name, created_at, updated_at)
-values('result',
-now(),
-now())
-;
+CREATE UNIQUE INDEX index_countries_on_name
+  ON countries
+  USING btree
+  (name COLLATE pg_catalog."default");
 
 
-INSERT INTO notification_types (
-name, created_at, updated_at)
-values('challenge',
-now(),
-now())
-;
 
-INSERT INTO notification_types (
-name, created_at, updated_at)
-values('news',
-now(),
-now())
-;
+-- Table: currencies
+
+-- DROP TABLE currencies;
+
+CREATE TABLE currencies
+(
+  id serial NOT NULL,
+  name character varying,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  CONSTRAINT currencies_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE currencies
+  OWNER TO ucfdkl13ogoaal;
 
 
-INSERT INTO notification_types (
-name, created_at, updated_at)
-values('personal',
-now(),
-now())
-;
+-- Table: payment_services
+
+-- DROP TABLE payment_services;
+
+CREATE TABLE payment_services
+(
+  id serial NOT NULL,
+  name character varying,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  CONSTRAINT payment_services_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE payment_services
+  OWNER TO ucfdkl13ogoaal;
+
+-- Index: index_payment_services_on_name
+
+-- DROP INDEX index_payment_services_on_name;
+
+CREATE UNIQUE INDEX index_payment_services_on_name
+  ON payment_services
+  USING btree
+  (name COLLATE pg_catalog."default");
+
+
+
+-- Table: t_deposits
+
+-- DROP TABLE t_deposits;
+
+CREATE TABLE t_deposits
+(
+  id serial NOT NULL,
+  coins integer,
+  user_id integer,
+  detail character varying,
+  currency_id integer,
+  country_id integer,
+  payment_service_id integer,
+  transaction_id character varying,
+  price double precision,
+  operator character varying,
+  type character varying,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  CONSTRAINT t_deposits_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_rails_3bb17d497f FOREIGN KEY (user_id)
+      REFERENCES users (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_rails_74201ea829 FOREIGN KEY (country_id)
+      REFERENCES countries (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_rails_bbfcd38b97 FOREIGN KEY (payment_service_id)
+      REFERENCES payment_services (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_rails_ee40538629 FOREIGN KEY (currency_id)
+      REFERENCES currencies (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE t_deposits
+  OWNER TO ucfdkl13ogoaal;
+
+-- Index: index_t_deposits_on_country_id
+
+-- DROP INDEX index_t_deposits_on_country_id;
+
+CREATE INDEX index_t_deposits_on_country_id
+  ON t_deposits
+  USING btree
+  (country_id);
+
+-- Index: index_t_deposits_on_currency_id
+
+-- DROP INDEX index_t_deposits_on_currency_id;
+
+CREATE INDEX index_t_deposits_on_currency_id
+  ON t_deposits
+  USING btree
+  (currency_id);
+
+-- Index: index_t_deposits_on_payment_service_id
+
+-- DROP INDEX index_t_deposits_on_payment_service_id;
+
+CREATE INDEX index_t_deposits_on_payment_service_id
+  ON t_deposits
+  USING btree
+  (payment_service_id);
+
+-- Index: index_t_deposits_on_user_id
+
+-- DROP INDEX index_t_deposits_on_user_id;
+
+CREATE INDEX index_t_deposits_on_user_id
+  ON t_deposits
+  USING btree
+  (user_id);
+
+
+
+-- Table: t_prizes
+
+-- DROP TABLE t_prizes;
+
+CREATE TABLE t_prizes
+(
+  id serial NOT NULL,
+  coins integer,
+  user_id integer,
+  detail character varying,
+  prize_id integer,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  CONSTRAINT t_prizes_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_rails_4d8589d70a FOREIGN KEY (prize_id)
+      REFERENCES prizes (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_rails_c1b16bfb73 FOREIGN KEY (user_id)
+      REFERENCES users (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE t_prizes
+  OWNER TO ucfdkl13ogoaal;
+
+-- Index: index_t_prizes_on_prize_id
+
+-- DROP INDEX index_t_prizes_on_prize_id;
+
+CREATE INDEX index_t_prizes_on_prize_id
+  ON t_prizes
+  USING btree
+  (prize_id);
+
+-- Index: index_t_prizes_on_user_id
+
+-- DROP INDEX index_t_prizes_on_user_id;
+
+CREATE INDEX index_t_prizes_on_user_id
+  ON t_prizes
+  USING btree
+  (user_id);
+
 
 
 
