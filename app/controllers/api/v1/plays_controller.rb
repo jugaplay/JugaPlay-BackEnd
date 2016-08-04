29bac56f662d8b1afae2d24cb331f49bf806b492
florencia <torielli.florencia@gmail.com>
2016-08-03 21:23:08 -0300

    plays correccion

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -3,10 +3,7 @@ class Api::V1::PlaysController < Api::BaseController
   def index
     @plays = Play.where(user: current_user).joins(:table).where("tables.opened = false").order("tables.start_time DESC").limit(params[:to]).offset(params[:from])
     @earn_coins = TPrize.where(user: current_user).sum("coins")
-    rescue ActiveRecord::RecordNotFound => e
-    	render json: { errors: e }, status: 422
-  	end
-  
+    
   end
 
 end
