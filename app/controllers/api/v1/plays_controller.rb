25387b7d47bcb0bc751ddef43fc171dd313b5a6e
florencia <torielli.florencia@gmail.com>
2016-08-03 21:09:54 -0300

    test para error ambiente testing

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::PlaysController < Api::BaseController
+
   def index
-    @plays = Play.where(user: current_user).limit(params[:to]).offset(params[:from])
+    @plays = Play.where(user: current_user).joins(:table).where("tables.opened = false").order("tables.start_time DESC").limit(params[:to]).offset(params[:from])
     @earn_coins = TPrize.where(user: current_user).sum("coins")
+    rescue ActiveRecord::RecordNotFound => e
+    	render json: { errors: e }, status: 422
+  	end
+  
   end
+
 end
