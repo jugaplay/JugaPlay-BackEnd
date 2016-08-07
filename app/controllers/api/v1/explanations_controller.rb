66f778c8cb2451f68430d1f48ca40da4741f45f0
florencia <torielli.florencia@gmail.com>
2016-08-07 10:08:00 -0300

    explanation

--- app/controllers/api/v1/explanations_controller.rb
+++ app/controllers/api/v1/explanations_controller.rb
@@ -2,18 +2,21 @@ class Api::V1::ExplanationsController < Api::BaseController
   skip_before_filter :authenticate_user!, only: [:create]
 
   def index
-    @explanations = Explanation.where(id: params[:user_id])
+    @explanations = Explanation.where(id: current_user)
   end
 
 
  def create
- 	@user = User.find(params[:user_id])
-    @explanation = Explanation.find(params[:explanation_id])
+ 
+ 	@user = User.find(current_user)
+    @explanation = Explanation.find(params[:id])
 
-    return render_json_errors @explanation.errors unless @explanation.save   
-    
-    @user.explanations<<(@explanation)
-    render :show
+    if @explanation.present? and @user.present?   
+	    @user.explanations<<(@explanation)
+    	render :show
+    else
+     	return render_json_errors 'Explanation not found'
+    end
     
   end
 
