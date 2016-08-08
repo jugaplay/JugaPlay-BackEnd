eb7c7ce68dc2b12989c1cbb988ff23fec1b310d0
florencia <torielli.florencia@gmail.com>
2016-08-07 23:15:41 -0300

    mailer

--- app/controllers/api/v1/mailer_controller.rb
+++ app/controllers/api/v1/mailer_controller.rb
@@ -7,16 +7,16 @@ class Api::V1::MailerController < ApplicationController
 	
 		@subject = "#{@user.nickname} te ha invitado a jugar a JugaPlay"
 		
-		if params[:to].present?	
+		if params[:to].present? and params[:from].present?	
 			params[:to].split(',').each do |to|
 				InvitationsMailer.send_mail(to,params[:from],params[:content],params[:from_user_id],params[:sender_link], @subject).deliver_now	  	
 				SentMail.create!(from: params[:from], to: to, subject: @subject)
 		    end
 	    else
-	   		return	render json: { errors: 'Parameter :to is invalid' }
+	   		return	render json: { errors: 'Parameters required: to and from' }
 	    end
 	else
-		  return 		render json: { errors: 'Parameter :from_user_id is invalid' }
+		  return 	render json: { errors: 'Parameter :from_user_id is invalid' }
 	
 	end
 	    
