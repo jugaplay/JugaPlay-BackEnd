ba7c8d0674a2c4edacec7d06be9536107cf7e0e7
florencia <torielli.florencia@gmail.com>
2016-08-11 21:40:36 -0300

    carga de invitations y promotions cuando se crea el usuario

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -12,46 +12,14 @@ class Api::V1::InvitationsController < ApplicationController
 
 	@request = Request.where(id: params[:request_id]).first()
 	@invitation_status = InvitationStatus.where(name: params[:invitation_status]).first()
+    
     @invitation = Invitation.new(invitation_status:@invitation_status , request: @request, won_coins: params[:won_coins], guest_user: @guest_user, detail: params[:detail])
 	
-    	return render :show if @invitation.save
-        render json: { errors: @invitation.errors }
+    return render :show if @invitation.save
+    render json: { errors: @invitation.errors }
   
   end
-
-  def update
-    
-    @invitation = Invitation.where(id: params[:id]).first()
-    
-    if(@invitation.present?)
-    
-	    if params[:guest_user_id].present?
-		 @guest_user = User.find(params[:guest_user_id])
-		 if params[:won_coins].present?
-	    	 @invitation.request.host_user.win_coins!(params[:won_coins])
-	     end
-	     @invitation.invitation_status = InvitationStatus.where(name: params[:invitation_status]).first()
-		end
-		
-	    return render :show if @invitation.update(update_invitation_params)    
-        render json: { errors: @invitation.errors }
-    
-    else
-    
-        render json: { errors: 'Invalid invitation_id' }
-    
-    end
-    
-    
-  end
-  
   
 
-private
-
-  def update_invitation_params
-	
-    params.permit(:won_coins, :detail, :guest_ip, :guest_user_id, @invitation_status)
-  end
   
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,13 @@ class Api::V1::UsersController < Api::BaseController
   def create
     @user = User.new(create_user_params)  
     return render_json_errors @user.errors unless @user.save
-    @user.invited_by.win_coins!(Wallet::COINS_PER_INVITATION) if @user.invited_by.present?
+    if @user.invited_by.present?
+	    @user.invited_by.win_coins!(Wallet::COINS_PER_INVITATION) 
+	    
+    	@invitation = Invitation.where(id: params[:user][:invitation_id]).first() if params[:user][:invitation_id].present?
+    	@invitation.update(:won_coins => Wallet::COINS_PER_INVITATION,:guest_user_id => @user.id, :guest_ip => request.remote_ip, :invitation_status => InvitationStatus.find_by_name('Registered') ) if @invitation.present?
+    	TPromotion.create!(coins: Wallet::COINS_PER_INVITATION, user: @user.invited_by, detail: 'Invitación a ' + @user.nickname, promotion_type: 'friend-invitation')
+    end
     WelcomeMailer.send_welcome_message(@user).deliver_now
     render :show
   end
