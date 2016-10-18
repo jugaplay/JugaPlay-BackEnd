d31c6d133bb9b826847c4c230ca43dfcf68ad9c7
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2016-10-18 12:17:38 -0300

    Fix unique index for user prizes migration

--- db/migrate/20161016004000_add_unique_index_to_user_prizes.rb
+++ db/migrate/20161016004000_add_unique_index_to_user_prizes.rb
@@ -2,7 +2,13 @@ class AddUniqueIndexToUserPrizes < ActiveRecord::Migration
   def change
     UserPrize.find_each do |user_prize|
       user_prizes = UserPrize.where(table_id: user_prize.table_id, user_id: user_prize.user_id)
-      user_prizes[1..-1].each(&:destroy) if user_prizes.many?
+      if user_prizes.many?
+        total_coins = user_prizes.pluck(:coins).sum
+        user_prize = user_prizes.first
+        user_prizes[1..-1].each(&:destroy)
+        puts "Actualizando los premios del usuario #{user_prize.user_id} con monedas #{user_prize.coins} a #{total_coins}"
+        user_prize.update_attributes(coins: total_coins)
+      end
     end
 
     add_index :user_prizes, [:table_id, :user_id], unique: true
