fdf2ee66e8d0c9736f144110ad39173e01842103
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2016-11-13 18:16:53 -0300

    Synchronize address book with phones

--- app/models/address_book_contact.rb
+++ app/models/address_book_contact.rb
@@ -5,11 +5,16 @@ class AddressBookContact < ActiveRecord::Base
   validates :user, presence: true
   validates :nickname, presence: true
   validates :synched_by_email, inclusion: { in: [ true, false ] }
+  validates :synched_by_phone, inclusion: { in: [ true, false ] }
   validates :synched_by_facebook, inclusion: { in: [ true, false ] }
   validates :address_book, presence: true, uniqueness: { scope: [:user] }
 
   def synched_by_email!
     update_attributes!(synched_by_email: true)
+  end
+
+  def synched_by_phone!
+    update_attributes!(synched_by_phone: true)
   end
 
   def synched_by_facebook!
