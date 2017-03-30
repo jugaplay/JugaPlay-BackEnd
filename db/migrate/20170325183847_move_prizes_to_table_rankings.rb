e2290bf5717767320145f292911c21deab3fce5a
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2017-03-29 21:29:12 -0300

    Improve move prizes to table rankings migration

--- db/migrate/20170325183847_move_prizes_to_table_rankings.rb
+++ db/migrate/20170325183847_move_prizes_to_table_rankings.rb
@@ -2,23 +2,27 @@ class MovePrizesToTableRankings < ActiveRecord::Migration
   def up
     add_column :table_rankings, :earned_coins, :integer, null: true
 
-    prizes_data = ActiveRecord::Base.connection.execute('SELECT table_id, user_id, coins, updated_at, created_at FROM prizes')
-    puts "Moving #{prizes_data.count} prizes to table rankings"
+    prizes_data = ActiveRecord::Base.connection.execute('
+      SELECT plays.id AS play_id, coins AS earned_coins, prizes.updated_at AS updated_at,
+             prizes.created_at AS created_at, table_rankings.id AS table_ranking_id
+      FROM prizes
+        INNER JOIN plays ON (prizes.table_id = plays.table_id AND prizes.user_id = plays.user_id)
+        INNER JOIN table_rankings ON (table_rankings.play_id = plays.id)
+    ')
+    puts "Preparing #{prizes_data.count} prizes to be moved to table rankings"
+
+    table_rankigns_ids, table_rankigns_data = [], []
     prizes_data.each do |prize_data|
-      coins = prize_data['coins']
-      user_id = prize_data['user_id']
-      table_id = prize_data['table_id']
-      updated_at = prize_data['updated_at']
-      created_at = prize_data['created_at']
-
-      play = Play.find_by(user_id: user_id, table_id: table_id)
-      fail "Missing play for user #{user_id} and table #{table_id}" unless play
-
-      table_ranking = play.table_ranking
-      fail "Missing table ranking for play #{play.id}" unless play unless table_ranking
-
-      table_ranking.update_attributes!(earned_coins: coins, created_at: created_at, updated_at: updated_at)
+      table_rankigns_ids << prize_data['table_ranking_id']
+      table_rankigns_data << {
+        earned_coins: (prize_data['earned_coins'] || 0),
+        created_at: prize_data['created_at'],
+        updated_at: prize_data['updated_at'],
+      }
     end
+
+    puts "Moving #{table_rankigns_data.count} prizes to table rankings"
+    TableRanking.update(table_rankigns_ids, table_rankigns_data)
 
     table_rankings_with_null_earned_coins = ActiveRecord::Base.connection.execute('SELECT id FROM table_rankings WHERE earned_coins IS NULL')
     puts "#{table_rankings_with_null_earned_coins.count} table rankings where found with null earned coins, setting to 0"
