1619ec079a5da371b3b12d335dfe60d5b336e0a1
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2017-04-05 18:48:18 -0300

    Improve create player selections migration

--- db/migrate/20170327113133_create_player_selection.rb
+++ db/migrate/20170327113133_create_player_selection.rb
@@ -12,21 +12,32 @@ class CreatePlayerSelection < ActiveRecord::Migration
     add_index :player_selections, [:play_id, :player_id], unique: true
 
     calculator = PlayerPointsCalculator.new
-    Table.find_each do |table|
-      puts "Calculating plays points for table #{table.id}"
-      player_selections = []
-      table.play_ids.each do |play_id|
-        players_data = ActiveRecord::Base.connection.execute("SELECT players_plays.player_id AS player_id FROM players_plays WHERE players_plays.play_id = #{play_id}")
-        puts "Calculating points for #{players_data.count} players of play #{play_id}"
-        players_data.each_with_index do |data, index|
-          player_id = data['player_id']
-          player = Player.find(player_id)
-          player_points = calculator.call(table, player)
-          player_selections << PlayerSelection.new(player_id: player_id, play_id: play_id, points: player_points, position: index + 1)
+    tables = Table.all.to_a
+
+    batch_size = 100
+    total = Play.count
+    batches = total/batch_size
+    Play.find_in_batches(batch_size: batch_size).with_index do |plays, batch|
+      puts "Importing player selections for #{batch_size} plays [#{batch}/#{batches}]"
+      plays.each do |play|
+        player_selections = []
+        table = tables.detect { |table| table.id.eql? play.table_id }
+        players_data = ActiveRecord::Base.connection.execute("SELECT players_plays.player_id AS player_id FROM players_plays WHERE players_plays.play_id = #{play.id}")
+        players_ids = players_data.map { |data| data['player_id'] }
+
+        Player.find(players_ids).each_with_index do |player, index|
+
+          player_points = begin
+            calculator.call(table, player)
+          rescue MissingPlayerStats
+            0
+          end
+          player_selections << PlayerSelection.new(player_id: player.id, play_id: play.id, points: player_points, position: index + 1)
         end
+
+        # puts "Importing #{player_selections.count} player selections for play #{play.id}"
+        PlayerSelection.import(player_selections)
       end
-      puts "Importing #{player_selections.count} player selections for table #{table.id}"
-      PlayerSelection.import(player_selections)
     end
 
     drop_table :players_plays
