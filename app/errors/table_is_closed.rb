49a91215b27de16233e2611a7f754651bdbf77d0
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2017-04-13 19:51:28 -0300

    Mejorar mensajes de error

--- app/errors/table_is_closed.rb
+++ app/errors/table_is_closed.rb
@@ -1,7 +1,7 @@
 class TableIsClosed < StandardError
   def self.for(table)
-    new "The table #{table.title} is already closed" if table.closed?
-    new "The table #{table.title} is being closed"
+    new "The table #{table.title} [#{table.id}] is already closed" if table.closed?
+    new "The table #{table.title} [#{table.id}] is being closed"
   end
 
   def initialize(message = 'The Table is already closed')
